a85356dad5e097e16f306c4f64efb7d7a8b3a099
FABIAN SCHEUCH <Fabian_Scheuch@sluz.ch>
2025-04-04 12:23:58 +0200

    add calculateProRataVacationDays and jest tests [STAI]

--- src/vacation-days.test.ts
+++ src/vacation-days.test.ts
@@ -16,3 +16,90 @@ test("full year 100% employment gives all vacation days", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("full leap year 100% employment gives all vacation days", () => {
+  // Arrange
+  const fullTime: Employment = {
+    startDate: new Date(Date.parse("2024-01-01 00:00:00+01:00")),
+    untilDate: new Date(Date.parse("2024-12-31 23:59:59+01:00")),
+    percentage: 100,
+    vacationDays: 25,
+  };
+  const expected = 25;
+
+  // Act
+  const actual = calculateProRataVacationDays(fullTime);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("full year 60% employment gives 15 vacation days", () => {
+  // Arrange
+  const partTime: Employment = {
+    startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+    untilDate: new Date(Date.parse("2025-12-31 23:59:59+01:00")),
+    percentage: 60,
+    vacationDays: 25,
+  };
+  const expected = 15;
+
+  // Act
+  const actual = calculateProRataVacationDays(partTime);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("46 days 100% employment gives 4 vacation days", () => {
+  // Arrange
+  const fullTime: Employment = {
+    startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+    untilDate: new Date(Date.parse("2025-02-15 23:59:59+01:00")),
+    percentage: 100,
+    vacationDays: 25,
+  };
+  const expected = 4;
+
+  // Act
+  const actual = calculateProRataVacationDays(fullTime);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+
+test("leap year 60 days 100% employment gives 5 vacation days", () => {
+  // Arrange
+  const partTime: Employment = {
+    startDate: new Date(Date.parse("2024-01-01 00:00:00+01:00")),
+    untilDate: new Date(Date.parse("2024-02-29 23:59:59+01:00")),
+    percentage: 100,
+    vacationDays: 25,
+  };
+  const expected = 5;
+
+  // Act
+  const actual = calculateProRataVacationDays(partTime);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("90 days 70% employment gives 5 vacation days", () => {
+  // Arrange
+  const partTime: Employment = {
+    startDate: new Date(Date.parse("2025-01-01 00:00:00+01:00")),
+    untilDate: new Date(Date.parse("2025-03-31 23:59:59+01:00")),
+    percentage: 70,
+    vacationDays: 25,
+  };
+  const expected = 5;
+
+  // Act
+  const actual = calculateProRataVacationDays(partTime);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
--- src/vacation-days.ts
+++ src/vacation-days.ts
@@ -1,3 +1,6 @@
+import { isLeapYear } from "./utils";
+
+
 export type Employment = {
   startDate: Date;
   untilDate: Date;
@@ -5,7 +8,19 @@ export type Employment = {
   vacationDays: number;
 };
 
+// Zeile 9 = Signatur, darf nicht verändert werden
 export function calculateProRataVacationDays(employment: Employment): number {
-  // TODO: calculate pro rata (consider workload and days worked)
-  return employment.vacationDays;
+  const dayInMillSec = 1000 * 60 * 60 * 24;
+  const diffStartUntilDatesInMillSec = Math.abs(employment.startDate.getTime() - employment.untilDate.getTime());
+  const employmentInDays = Math.floor(diffStartUntilDatesInMillSec / dayInMillSec);
+
+  const isALeapYear = isLeapYear(employment.startDate.getFullYear());
+  
+  let yearInDays = 365;
+  if (isALeapYear) {
+    yearInDays = 366;
+  } 
+  
+  const actualVacationDays = (employment.vacationDays * (employmentInDays/yearInDays) * (employment.percentage/100));
+  return Math.ceil(actualVacationDays);
 }
